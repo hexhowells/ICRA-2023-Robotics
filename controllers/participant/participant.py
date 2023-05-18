6b593dc55f935a90e580c1352a7e5bf951a9537c
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-05-18 09:00:53 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -110,7 +110,7 @@ class HexBot (Robot):
                 continue
 
             if 0.3 < t < 1.5:
-                self.start_sequence()
+                #self.start_sequence()
             elif t > 1.5:
                 self.fall_detector.check()
                 #self.position_arms()
