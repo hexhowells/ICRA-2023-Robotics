a780caf558b98a1209eb020475ee5f5fed80352b
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-05-18 10:26:22 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -105,7 +105,7 @@ class HexBot (Robot):
             self.gait_manager.update_theta()
 
             
-            if t > 4:
+            if t > 35:
                 self.fall_detector.check()
                 continue
 
