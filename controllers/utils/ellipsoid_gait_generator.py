c5d7234db612765ff94e46cedfda3245822f17d9
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-05-19 23:15:57 +0100

    Update gait generator parameters

--- controllers/utils/ellipsoid_gait_generator.py
+++ controllers/utils/ellipsoid_gait_generator.py
@@ -43,8 +43,8 @@ class EllipsoidGaitGenerator():
         self.roll_reflex_factor = 4e-2  # h_VSR in the paper
         # the force reflex factor is h_ER/(mass*gravity) in the paper
         self.force_reflex_factor = 1e-2 / (5.305 * 9.81)
-        self.robot_height_offset = 0.31  # desired height for the robot's center of mass
-        self.lateral_leg_offset = 0.05  # y distance between the center of mass and one foot
+        self.robot_height_offset = 0.29  # desired height for the robot's center of mass
+        self.lateral_leg_offset = 0.06  # y distance between the center of mass and one foot
         self.step_period = 0.3  # 0.4  # time to complete one step
         # amplitudes of stride:
         self.step_length_front = self.MAX_STEP_LENGTH_FRONT  # when heading in the front direction (x axis)
