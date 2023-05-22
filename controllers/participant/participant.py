364b8d348b509abce059503972e8dd070f04a398
Morgan <58524460+hexhowells@users.noreply.github.com>
2023-05-22 14:51:07 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -26,7 +26,6 @@ class HexBot (Robot):
         self.cameraBottom = Camera(self, camera_name='CameraBottom')
         self.fall_detector = FallDetection(self.time_step, self)
         self.gait_manager = GaitManager(self, self.time_step)
-        self.heading_angle = 3.14 / 2
         self.opponent_x = [0]*10
 
         # used for detecting the edge of the ring
